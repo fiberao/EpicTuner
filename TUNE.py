5a8ee2e3dfbd3c2c12cd996ecd23b9d4fa2d40d4
Zuzeng Lin <linzuzeng@hotmail.com>
2018-05-07 16:31:48 +0200

    Signed-off-by: Zuzeng Lin <linzuzeng@hotmail.com>

--- TUNE.py
+++ TUNE.py
@@ -1,4 +1,4 @@
-from optimizer import genetic
+from optimizer import genetic,nm
 import feedback
         
 
@@ -10,13 +10,12 @@ if __name__ == "__main__":
     else:
         print("raw optimization running...")
         feedback = feedback_raw
-    initial_trubulance=float(input("initial_trubulance(0.1):"))
-
     if input("ga/nm?:").find("ga")>=0:
         print("optimization started with Nelder_mead")
-        final = nelder_mead(feedback.f, feedback.acturator.read(), max_iter=10000)
+        final = nm.nelder_mead(feedback.f, feedback.acturator.read(), max_iter=10000)
         feedback.f(final[0])
     else:
+        initial_trubulance=float(input("initial_trubulance(0.1):"))
         print("optimization started with Genetic algorithm")
         genetic.genetic(feedback.f, feedback.acturator.read().tolist(),
                 feedback.acturator.min.tolist(), feedback.acturator.max.tolist(),
