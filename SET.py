c6b20ac938a44c6d562e7bc7f16e6137937aaab8
Zuzeng Lin <linzuzeng@hotmail.com>
2018-05-07 11:31:52 +0200

    Signed-off-by: Zuzeng Lin <linzuzeng@hotmail.com>

--- SET.py
+++ SET.py
@@ -10,18 +10,22 @@ The above copyright notice and this permission notice shall be included in all c
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
 
-import feedback
-import pickle
-import numpy as np
+import feedback,json
+
 if __name__ == "__main__":
     feedback,feedback_znk = feedback.create_loop(prefix="usersetpoint")
     chn = feedback.acturator.chn
     while True:
-        fname = input("set value to all chn:")
-        with open(fname + ".txt", 'r') as output:
-            if fname.find("znk")>0:
-                feedback_znk.write(json.load(output))
-            if fname.find("raw")>0:
-                feedback.write(json.load(output))
-        # print(feedback.mirrors_now)
-        print("Power: {} uW".format(feedback.sensor.read()))
+        try:
+            fname = input("set value to all chn:")
+            with open(fname + ".txt", 'r') as output:
+                if fname.find("znk")>0:
+                    print("zernike setpoint")
+                    feedback_znk.write(json.load(output))
+                if fname.find("raw")>0:
+                    print("raw setpoint")
+                    feedback.write(json.load(output))
+            # print(feedback.mirrors_now)
+            print("Power: {} uW".format(feedback.sensor.read()))
+        except Exception as e:
+            print(e)
--- TUNE.py
+++ TUNE.py
@@ -2,9 +2,13 @@ from optimizer import genetic
 import feedback
 
 if __name__ == "__main__":
-    feedback,feedback_znk = feedback.create_loop()
-
-    print("raw optimization running...")
+    feedback_raw,feedback_znk = feedback.create_loop()
+    if(input("znk/raw?:").find("znk">=0)):
+        feedback = feedback_znk
+        print("znk optimization running...")
+    else:
+        print("raw optimization running...")
+        feedback = feedback_raw
     genetic.genetic(feedback.f, feedback.acturator.read().tolist(),
             feedback.acturator.min.tolist(), feedback.acturator.max.tolist(),
             goal=80000, initial_trubulance=0.10)
